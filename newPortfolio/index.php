05750fa69cc26178661bd7458ec773659c8a8705
elies <elieskedim23@gmail.com>
2018-11-09 16:34:20 +0100

    portfolio

---
+++ newPortfolio/index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>Elies Kedim - Développeur web</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<header class="bg-white pt-1">
+    <div class="col-md pb-2">
+        <h1 class="text-center">Elies Kedim</h1>
+        <div class="text-center">Développeur | Intégrateur Web</div>
+    </div>
+</header>
+
+
+<section class="mt-5 container">
+    <div class="text-center"><img class="logo" src="img/work.png" alt=""></div>
+     <div class="text-white text-center"><i class="fas fa-quote-left"></i>  Il y en a encore beaucoup d'autres ! On pourras en discuter lors d'un entretient..  <i class="fas fa-quote-right"></i></div>
+    <div class="container bg-white p-3">
+    <div class="row">
+        <div class="col-md-6">
+            <h2 class="mb-3">Compétences Front-End</h2>
+            <h3 class="progress-title">HTML5/CSS3</h3>
+            <div class="progress">
+                <div class="progress-bar" style="width:90%; background:#e96329;">
+                </div>
+                <div class="progress-value text-dark text-right">90%</div>
+            </div>
+ 
+            <h3 class="progress-title mt-2">Bootstrap</h3>
+            <div class="progress">
+                <div class="progress-bar" style="width:80%; background:#f2545b;">
+                    
+                </div>
+                <div class="progress-value text-dark text-right">80%</div>
+            </div>
+            
+            <h3 class="progress-title mt-2">Javascript / jQuery</h3>
+            <div class="progress">
+                <div class="progress-bar" style="width:80%; background:#ffdd23;">
+                </div>
+                    <div class="progress-value text-dark text-right">80%</div>
+            </div>
+            
+            <h3 class="progress-title mt-2">Angular6</h3>
+            <div class="progress">
+                <div class="progress-bar" style="width:20%; background:#21ff63;">
+                </div>
+                    <div class="progress-value text-dark text-right">20%</div>
+            </div>
+        </div>
+
+
+        <div class="col-md-6">
+            <h2 class="mb-3">Compétences Back-End</h2>
+            <h3 class="progress-title mt-2">PHP</h3>
+            <div class="progress">
+                <div class="progress-bar" style="width:70%; background:#4286f4;">
+                </div>
+                <div class="progress-value text-dark text-right">80%</div>
+            </div>
+ 
+            <h3 class="progress-title mt-2">SQL</h3>
+            <div class="progress">
+                <div class="progress-bar" style="width:100%; background:#ef8d1d;">
+                    
+                </div>
+                <div class="progress-value text-dark text-right">70%</div>
+            </div>
+            
+            <h3 class="progress-title mt-2">Python</h3>
+            <div class="progress">
+                <div class="progress-bar" style="width:80%; background:#323234;">
+                    </div>
+                    <div class="progress-value text-dark  text-left">60%</div>
+            </div>
+            
+            <h3 class="progress-title mt-2">Wordpress</h3>
+            <div class="progress">
+                <div class="progress-bar" style="width:90%; background:silver;">
+                    <div class="progress-value text-dark text-left">90%</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+
+<section class="bg-white mt-5 container-fluid" style="height:600px;"><!-- Bloc portfolio -->
+    <div class="row">
+        <div class="col-md-5">
+            <img class="logo" src="img/unnamed.png" alt="">
+            <span class="text-grey"><i class="fas fa-quote-left"></i>  J'ai fait tous ça en 5 minutes..  <i class="fas fa-quote-right"></i></span>
+        </div>
+        <div class="col-md-6"><h2 class="ml-5">Portfolio</h2></div>
+    </div>
+</section>
+
+<section class="bg-white mt-5 container-fluid" style="height:600px;"><!-- Bloc Exp pro -->
+    <div class="row">
+        <div class="col-md-8"><h2 class="text-center">Portfolio</h2></div>
+        <div class="col-md-4">
+            <span class="text-grey"><i class="fas fa-quote-left"></i>  J'ai aussi effectué des petits jobs  <i class="fas fa-quote-right"></i></span>
+            <img id="img" class="logo" src="img/pro.png" alt="">
+        </div>
+    </div>
+    
+</section>
+
+<section class="mt-5 mb-5 container-fluid">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-5 bg-white ml-2" style="height:300px;">
+                <h2 class="text-center">Film/Séries</h2>
+            </div>
+            <img class="logo ml-5 mt-5" src="img/lvlup.png" alt="">
+            <div class="col-md-5 bg-white ml-5" style="height:300px;"><h2 class="text-center">Jeux</h2></div>
+        </div>
+    </div>
+</section>
+
+<section class="mt-5 mb-5 container bg-white">
+<h2 class="text-center mb-5">A propos de moi</h2>
+<div class="row">
+    <div class="col-md">
+        <h3>Lui c'est mon meilleur Ami</h3>
+        <img class="logo ml-5 mt-5" src="img/linux.png" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam corrupti dolore obcaecati ab repellat, architecto quasi laboriosam, dignissimos laborum vero eligendi dolorem sapiente porro, omnis officia voluptatum nam modi ducimus!</p>
+    </div>
+    <div class="col-md">
+        <h3>Ca c'est moi, enfin je crois...</h3>
+        <img class="logo ml-5 mt-5" src="img/wow.jpg" alt="">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam corrupti dolore obcaecati ab repellat, architecto quasi laboriosam, dignissimos laborum vero eligendi dolorem sapiente porro, omnis officia voluptatum nam modi ducimus!</p>
+    </div>
+</div>
+</section>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>

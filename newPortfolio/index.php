c88138015361221a111aebd43fc46cb1f5260df9
elies <elieskedim23@gmail.com>
2018-12-04 09:16:41 +0100

    04/12/2018

--- newPortfolio/index.php
+++ newPortfolio/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     <title>Elies Kedim - Développeur web</title>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet"> 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/style.css" />
@@ -17,8 +18,11 @@
     </div>
 </header>
 
+<aside id="formClick" class="pt-4">
+    <p class="text-center"><i class="fas fa-info"></i></p>
+</aside>
 
-<section class="mt-5 container">
+<section class="mt-5 container morph">
     <div class="text-center"><img class="logo" src="img/work.png" alt=""></div>
      <div class="text-white text-center mb-3"><i class="fas fa-quote-left"></i>  Il y en a encore beaucoup d'autres ! On pourras en discuter lors d'un entretient..  <i class="fas fa-quote-right"></i></div>
     <div class="container bg-white p-3 test">
@@ -78,27 +82,57 @@
 <section id="media" class="mt-5 mb-5 container-fluid">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-5 bg-white ml-2 pb-5 test">
+            <div class="col-md-5 bg-white pb-5 test">
                 <h2 class="text-center mb-5">Film/Séries</h2>
                 <div class="text-center">
-                    <img width="100px;" class="" src="img/film/fb2.jpg" alt="">
-                    <img width="100px;" class="ml-md-3" src="img/film/halloween.jpg" alt="">
-                    <img width="100px;" height="137px;" class="ml-md-3" src="img/film/brooklyn.jpg" alt="">
-                    <img width="100px;" height="137px;" class="ml-md-3" src="img/film/myhero.jpg" alt="">
-                    <img width="100px;" height="137px;" class="ml-md-3" src="img/film/nonne.jpg" alt="">
+                    <a href="">
+                        <img width="100px;" class="" src="img/film/fb2.jpg" alt="">
+                        <span>Film Génial ! =)</span>
+                    </a>
+                    <a href="">
+                        <img width="100px;" src="img/film/halloween.jpg" alt="">
+                        <span>Long et ennuyeux !!</span>
+                    </a>
+                    <a href="">
+                        <img width="100px;" height="137px;" src="img/film/brooklyn.jpg" alt="">
+                        <span>J'aime beaucoup cette série ^_^</span>
+                    </a>
+                    <a href="">
+                        <img width="100px;" height="137px;" src="img/film/myhero.jpg" alt="">
+                        <span>Plus ultra !</span>
+                    </a>
+                    <a href="">
+                        <img width="100px;" height="137px;" src="img/film/nonne.jpg" alt="">
+                        <span>Nul à chier, je me suis fait volé ! alors que je l'ai craqué!!!</span>
+                    </a>
                 </div>
             </div>
-            <div class="text-center col mt-1">
+            <div class="text-center col-md-2 mt-1">
                 <img class="logo" src="img/lvlup.png" alt="">
             </div>
-            <div class="col-md-5 bg-white ml-5 pb-5 test">
-                <h2 class="text-center mb-5">Jeux</h2>
+            <div class="col-md-5 bg-white pb-5 test">
+                <h2 class="text-center mb-5">Dériner Jeux</h2>
                 <div class="text-center">
-                    <img width="100px;" height="137px;" class="ml-md-3" src="img/games/reddead.jpg" alt="">
-                    <img width="100px;" height="137px;" class="ml-md-3" src="img/games/civ.jpg" alt="">
-                    <img width="100px;" height="137px;" class="ml-md-3" src="img/games/divinity.jpg" alt="">
-                    <img width="100px;" height="137px;" class="ml-md-3" src="img/games/wow.jpg" alt="">
-                    <img width="100px;" height="137px;" class="ml-md-3" src="img/games/spiderman.jpg" alt="">
+                    <a href="">
+                        <img width="100px;" height="137px;"  src="img/games/reddead.jpg" alt="">
+                        <span>Je n'ai pas le temps d'y jouer :'(</span>
+                    </a>
+                    <a href="">
+                        <img width="100px;" height="137px;"  src="img/games/civ.jpg" alt="">
+                        <span>Je gagne à tous les coups!... en mode colon </span>
+                    </a>
+                    <a href="">
+                        <img width="100px;" height="137px;"  src="img/games/divinity.jpg" alt="">
+                        <span>Ma fait retomber dans mes années Baldur's</span>
+                    </a>
+                    <a href="">
+                        <img width="100px;" height="137px;"  src="img/games/wow.jpg" alt="">
+                        <span>Pour la Horde !</span>
+                    </a>
+                    <a href="">
+                        <img width="100px;" height="137px;"  src="img/games/spiderman.jpg" alt="">
+                        <span>J'aime bien se jeux </span>
+                    </a>
                 </div>
             </div>
         </div>
@@ -106,69 +140,17 @@
         
     </div>
 </section>
-<div id="blocPanda">
-    <div id="panda" style="height:300px;">
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 800 600" xml:space="preserve">
-      <filter id="shadowBlur" x="0%" y="0%" width="275%" height="150%">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="2"/>
-      </filter>    
-      <g id="shadow" opacity="0.4" filter="url(#shadowBlur)">
-        <ellipse fill="#54AEA7" cx="394.676" cy="343.597" rx="65" ry="8"/>
-      </g>
-      <g id="body">
-        <path id="frontPawR" fill="#303030" d="M369,338.5c0,3.59-6.716,6.5-15,6.5c-11,0-16-3.41-16-7s1-6,16-6C362.284,332,369,334.91,369,338.5z"/>
-        <path fill="#FFFFFF" d="M417.562,346l13.759-0.251c7.796-1.072,14.679-6.179,14.679-14.11v0c0-13.608-11.473-24.64-25.625-24.64H394v16.344C394,335.857,394,346,417.562,346z"/>
-        <path fill="#EBEBEB" d="M410.441,332.708c0,0,10.042,7.749,17.507,0.97c7.465-6.779,15.351-13.06,15.351-13.06l0.549,1.078
-                                c1.604,3.148,2.363,6.659,2.202,10.189L446,333c0,0-3.22,7.407-9.409,11.173c-2.231,1.357-6.845,1.88-9.024,1.921
-                                c-12.467,0.236-17.187-0.479-17.187-0.479l-3.517-7.829L410.441,332.708z"/>
-        <path fill="#303030" d="M445.981,332.269c0,0-0.629,1.396-1.693,2.21c-2.136,1.633-4.56,1.549-4.56,1.549l4.319,1.844
-                                c0.776,0.646,1.953,0.095,1.953-0.915L445.981,332.269z"/>
-        <path id="backPaw" fill="#303030" d="M452,343c0,3-5.149,3-11.5,3c-5.268,0-8.23,0-9.823-0.512c-1.28-0.411-1.677-1.152-1.677-2.488c0-4,7-7,11.5-7C446.851,336,452,340.239,452,343z"/>
-      </g>
-      <g id="head">
-        <path fill="#303030" d="M343,316c0,7.732,5.791,14,8,14s0-6.268,0-14s-0.791-14-3-14S343,308.268,343,316z"/>
-        <path fill="#FFFFFF" d="M383.9,279.5h-4.3c-18.28,0-33.1,14.819-33.1,33.1v4.958c0,1.512,0.124,2.995,0.355,4.442h-0.14
-                                c-3.882,0-4.939,2.615-4.819,6.698c0.217,7.382,1.104,16.802,32.615,16.802h9.589c18.17,0,32.9-14.73,32.9-32.9v0
-                                C417,294.319,402.181,279.5,383.9,279.5z"/>
-        <path fill="#EBEBEB" d="M381.186,279.5h2.896c18.281,0,33.1,14.819,33.1,33.1v0c0,11.356-5.754,21.369-14.505,27.281
-                                C402.677,339.881,411.009,298.46,381.186,279.5z"/>
-        <g id="snout">
-          <path fill="#231D1D" d="M348.974,328.117c-0.546,0.459-1.402,0.459-1.948,0l-1.603-1.348l0,0
-                                  c-0.928-0.781-0.305-2.167,0.974-2.167H348h1.603c1.279,0,1.902,1.386,0.974,2.167l0,0L348.974,328.117z"/>
-          <ellipse fill="#231D1D" cx="348.003" cy="325.855" rx="3.018" ry="1.717"/>
-        </g>
-        <path id="mouth" fill="none" stroke="#231D1D" stroke-width="2" stroke-linecap="round" stroke-miterlimit="10" d="M348,327v2.036c0,0,0,3.016,2.587,3.016c1.807,0,2.333-1.988,2.333-1.988"/>
-        <g id="ear">
-          <path fill="#231D1D" d="M416.259,314.576L416.259,314.576c6.679-6.187,7.078-16.616,0.891-23.295l-3.126-3.374
-                                  c-6.173-6.664-16.579-7.062-23.242-0.889l0,0c-3.892,3.605-4.124,9.683-0.519,13.575l12.475,13.467
-                                  C406.328,317.936,412.382,318.167,416.259,314.576z"/>
-          <ellipse transform="matrix(0.7071 -0.7071 0.7071 0.7071 -95.1679 373.1731)" fill="#3E3E3E" cx="402.876" cy="301.464" rx="11.926" ry="18.708"/>
-          <path fill="#3E3E3E" d="M416.43,314.417L416.43,314.417c4.294-3.977,4.55-10.682,0.573-14.976l-11.127-12.012
-                                  c-3.963-4.278-10.645-4.534-14.923-0.571l-0.916,0.849c-3.48,3.224-3.688,8.659-0.464,12.14l13.006,14.04
-                                  C406.257,317.859,412.459,318.096,416.43,314.417z"/>
-        </g>
-        <g>
-          <path fill="#EBEBEB" d="M355.5,317.014c0.084,2.508,0.581,15.98,16.458,18.427c3.678,0.567,7.472,0.262,10.927-1.118
-                                  c0.766-0.306,1.53-0.667,2.234-1.091c5.302-3.195,4.881-13.996,4.881-13.996c0-12.265-7.66-22.207-23.902-22.207
-                                  C356.756,297.027,355.5,307,355.5,317.014z"/>
-          <path fill="#302F2E" d="M355.5,315.5c0,9.665,3,16,16,16c12,0,14.5-4.335,14.5-14s-6.5-18-20.25-17.5
-                                  C356.781,300.326,355.5,305.835,355.5,315.5z"/>
-        </g>
-        <g id="EyeGroup">
-          <ellipse id="eyeBot" fill="#FFFFFF" cx="365.752" cy="320.027" rx="5.164" ry="5.053"/>
-          <circle id="eyeTop" fill="#302F2E" cx="365.752" cy="320.027" r="5.5"/>
-        </g>
-      </g>
-      <path id="frontPawL" fill="#303030" d="M417,340.5c0,4.142-5,5.5-12.5,5.5c-6.904,0-12.5,0-12.5-5.5c0-4.142,5.596-7.5,12.5-7.5S417,336.358,417,340.5z"/>
-      <g id="sleep">
-        <polyline fill="none" stroke="#303030" stroke-width="0.725" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="370.913,265.143 374.848,265.143 370.662,269.56 375.408,269.56"/>
-        <polyline fill="none" stroke="#303030" stroke-width="0.725" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="377.959,261.272 381.894,261.272 377.708,265.688 382.454,265.688"/>
-        <polyline fill="none" stroke="#303030" stroke-width="0.725" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" points="385.469,256.856 389.404,256.856 385.218,261.272 389.964,261.272"/>
-      </g>
-    </svg>
-    </div>
-</div>
-<section class="mt-5 mb-5 container">
+
+<footer class="bg-dark text-center text-white pt-3">
+    <span class="col">ELIES KEDIM
+    - Développeur Web</span>
+    <span class="col">07.69.49.90.39 | elieskedim1@gmail.com</span>
+    <span><a href=""><i class="fab fa-github"></i></a></span>
+    <span><a href=""><i class="fab fa-linkedin"></i></a></span>
+
+</footer>
+<!-- Formulaire -->
+<section id="form" class="mt-5 mb-5 container  modal p-3">
     <h2 class="text-center text-white">Me contacter</h2>
         <form class="text-white">
             <div class="form-group">
@@ -185,6 +167,10 @@
             </div>
             <button type="submit" class="btn btn-primary col">Submit</button>
         </form>
+        <div class="text-center text-white mt-5 mb-3">
+            <h3>Télécharger Mon CV</h3>
+            <a href=""><i class="fas fa-download text-white"></i></a>
+        </div>
 </section>
 
 <div id="modal0" class="modal">
@@ -307,7 +293,6 @@
     </div>
 </div>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.0.2/TweenMax.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
